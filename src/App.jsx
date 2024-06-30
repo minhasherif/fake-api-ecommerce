fe484ed6cf9e9da1d7f84c9a8787d03ce41a67d1
minhasherif <minhasherif4@gmail.com>
2024-06-30 14:29:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ResponsiveAppBar from './assets/components/ResponsiveAppBar'
+import RecipeReviewCard from './assets/components/RecipeReviewCard'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <RecipeReviewCard />
+    </div>
+  )
+}
+
+export default App
